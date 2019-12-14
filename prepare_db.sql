f2e39e502ddf32263070bee7a194163ab3c74b49
root <root@DESKTOP-KTKAT5F.localdomain>
2019-12-14 21:11:14 +0100

    Add quota for oracle user

--- prepare_db.sql
+++ prepare_db.sql
@@ -1,2 +1,3 @@
 CREATE USER chatbotts IDENTIFIED BY chooseapasswordandinserthere;
 GRANT CREATE TABLE, DELETE ANY TABLE, INSERT ANY TABLE, SELECT ANY TABLE, CREATE SESSION to chatbotts; 
+GRANT unlimited tablespace to chatbotts;
